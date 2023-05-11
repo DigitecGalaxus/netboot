4c4e5422da4c53cf1cd1ce5a7e209abd5e0d4fae
mazlumtoprak <mazlumtoprak@gmail.com>
2023-05-11 17:16:57 +0200

    add correct syncfolders and allways pull latest

--- netboot-services/sync/syncer.sh
+++ netboot-services/sync/syncer.sh
@@ -2,7 +2,7 @@
 
 #import environment variables
 
-if [ -z "$SAS_TOKEN" ]; then
+if [ -z "$SYNC_SAS_TOKEN" ]; then
   echo "SAS_TOKEN is not set"
   exit 1
 fi
@@ -10,12 +10,12 @@ fi
 while true
 do
   if [ "$SYNC_DEV" ]; then
-    azcopy sync "https://thinclientsimgstore.blob.core.windows.net/dev/$SAS_TOKEN" "/home/syncer/"
+    azcopy sync "https://thinclientsimgstore.blob.core.windows.net/dev/$SYNC_SAS_TOKEN" "/home/syncer/dev/"
   fi
 
-  azcopy sync "https://thinclientsimgstore.blob.core.windows.net/prod/$SAS_TOKEN" "/home/syncer/"
+  azcopy sync "https://thinclientsimgstore.blob.core.windows.net/prod/$SYNC_SAS_TOKEN" "/home/syncer/prod"
 
-  #random sleep every 5 minutes
+  #random sleep within 5 minutes
   sleep $(( ( RANDOM % 300 )  + 1 ))
   echo "Syncing run processed on $(date)"
 done
