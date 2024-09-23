ada320af863bf324908f44473cd66596ec8dc5f1
Mazlum Toprak <mazlumtoprak@gmail.com>
2024-09-23 16:54:45 +0200

    decouple cleaner tests

--- netboot-services/cleaner/main_test.go
+++ netboot-services/cleaner/main_test.go
@@ -1,144 +1,92 @@
 package main
 
 import (
-	"io/fs"
 	"os"
-	"sort"
+	"path/filepath"
 	"testing"
-	"time"
 
-	"gotest.tools/v3/assert"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestSortByModificationDate(t *testing.T) {
+func TestGetImagesSortedByModifiedDate(t *testing.T) {
+	// Arrange
+	tempDir := t.TempDir()
+	createTestImageFolder(t, tempDir, "image1")
+	createTestImageFolder(t, tempDir, "image2")
+	createTestImageFolder(t, tempDir, "image3")
 
-	allFiles := []fs.DirEntry{
-		//in random order
-		&mockDirEntry{name: "file2.txt", isDir: false, fileInfo: &mockFileInfo{name: "file2.txt", modTime: time.Now().Add(-1 * time.Hour)}},
-		&mockDirEntry{name: "file1.txt", isDir: false, fileInfo: &mockFileInfo{name: "file1.txt", modTime: time.Now()}},
-		&mockDirEntry{name: "file5.txt", isDir: false, fileInfo: &mockFileInfo{name: "file5.txt", modTime: time.Now().Add(-4 * time.Hour)}},
-		&mockDirEntry{name: "file4.txt", isDir: false, fileInfo: &mockFileInfo{name: "file4.txt", modTime: time.Now().Add(-3 * time.Hour)}},
-		&mockDirEntry{name: "file3.txt", isDir: false, fileInfo: &mockFileInfo{name: "file3.txt", modTime: time.Now().Add(-2 * time.Hour)}},
+	// Act
+	images := getImagesSortedByModifiedDate(tempDir)
+
+	// Assert
+	assert.Len(t, images, 3)
+	assert.Equal(t, "image3", images[0].Name())
+	assert.Equal(t, "image2", images[1].Name())
+	assert.Equal(t, "image1", images[2].Name())
+}
+
+func TestGetCurrentFolderSizeInGiB(t *testing.T) {
+	// Arrange
+	tempDir := t.TempDir()
+	createTestImageFolder(t, tempDir, "image1")
+	createTestImageFolder(t, tempDir, "image2")
+
+	// Act
+	size := getCurrentFolderSizeInGiB(tempDir)
+
+	// Assert
+	assert.Greater(t, size, 0.0)
+	assert.Less(t, size, 1.0) // Assuming test files are small
+}
+
+func TestFolderNeedsCleanup(t *testing.T) {
+	// Arrange
+	tempDir := t.TempDir()
+	properties := folderProperties{
+		FolderPath:              tempDir,
+		ThresholdMaxImagesCount: 2,
+		MaxFolderSizeInGiB:      0.1,
 	}
+	createTestImageFolder(t, tempDir, "image1")
+	createTestImageFolder(t, tempDir, "image2")
+	createTestImageFolder(t, tempDir, "image3")
 
-	sort.Sort(ByModTime(allFiles))
+	// Act & Assert
+	images := getImagesSortedByModifiedDate(tempDir)
+	folderSize := getCurrentFolderSizeInGiB(tempDir)
+	assert.True(t, folderNeedsCleanup(properties, folderSize, images))
 
-	assert.Equal(t, allFiles[0].Name(), "file1.txt")
-	assert.Equal(t, allFiles[1].Name(), "file2.txt")
-	assert.Equal(t, allFiles[2].Name(), "file3.txt")
-	assert.Equal(t, allFiles[3].Name(), "file4.txt")
-	assert.Equal(t, allFiles[4].Name(), "file5.txt")
-}
+	err := os.RemoveAll(filepath.Join(tempDir, "image3"))
+	require.NoError(t, err)
 
-type mockDirEntry struct {
-	name     string
-	isDir    bool
-	fileInfo fs.FileInfo
-}
-
-func (m *mockDirEntry) Name() string {
-	return m.name
-}
-
-func (m *mockDirEntry) IsDir() bool {
-	return m.isDir
-}
-
-func (m *mockDirEntry) Type() fs.FileMode {
-	// Implement this method if necessary
-	// Return a default or desired file mode
-	return fs.ModeDir
-}
-
-func (m *mockDirEntry) Info() (fs.FileInfo, error) {
-	return m.fileInfo, nil
-}
-
-type mockFileInfo struct {
-	name    string
-	size    int64
-	mode    fs.FileMode
-	modTime time.Time
-	isDir   bool
-}
-
-func (m *mockFileInfo) Name() string {
-	return m.name
-}
-
-func (m *mockFileInfo) Size() int64 {
-	return m.size
-}
-
-func (m *mockFileInfo) Mode() fs.FileMode {
-	return m.mode
-}
-
-func (m *mockFileInfo) ModTime() time.Time {
-	return m.modTime
-}
-
-func (m *mockFileInfo) IsDir() bool {
-	return m.isDir
-}
-
-func (m *mockFileInfo) Sys() interface{} {
-	return nil
-}
-
-func setupTestFiles(filename string, fileContent []byte) {
-	os.WriteFile(filename, fileContent, 0644)
-}
-
-func setupTestDirectories(dirs ...string) {
-	for _, dir := range dirs {
-		os.MkdirAll(dir, 0755)
-	}
-}
-
-func cleanupTestDirectories(dirs ...string) {
-	for _, dir := range dirs {
-		os.RemoveAll(dir)
-	}
+	images = getImagesSortedByModifiedDate(tempDir)
+	folderSize = getCurrentFolderSizeInGiB(tempDir)
+	assert.False(t, folderNeedsCleanup(properties, folderSize, images))
 }
 
 func TestDeleteImage(t *testing.T) {
-	setupTestDirectories("test", "test/img1", "test/img2", "test/img3")
-	setupTestFiles("test/img1/img1.squashfs", []byte("test1"))
-	setupTestFiles("test/img2/img2.squashfs", []byte("test2"))
-	setupTestFiles("test/img3/.azDownload-img3.squashfs", []byte("test3"))
+	// Arrange
+	tempDir := t.TempDir()
+	imageName := "image1"
+	createTestImageFolder(t, tempDir, imageName)
 
-	propertiesTest := folderProperties{
-		FolderPath:              "test",
-		ThresholdMaxImagesCount: 1,
-		MaxFolderSizeInGiB:      0.0000000000001, // small value due to small testsize
-	}
+	// Act
+	images := getImagesSortedByModifiedDate(tempDir)
+	require.Len(t, images, 1)
+	err := deleteImage(tempDir, images[0])
 
-	// should only get two images as one is .azDownload prefixed
-	testImages := getImagesSortedByModifiedDate(propertiesTest.FolderPath)
-	assert.Equal(t, 2, len(testImages))
+	// Assert
+	assert.NoError(t, err)
+	_, err = os.Stat(filepath.Join(tempDir, imageName))
+	assert.True(t, os.IsNotExist(err))
+}
 
-	folderSizeInGiB := getCurrentFolderSizeInGiB(propertiesTest.FolderPath)
-	for i := len(testImages) - 1; folderNeedsCleanup(propertiesTest, folderSizeInGiB, testImages); i-- {
-		err := deleteImage(propertiesTest.FolderPath, testImages[i])
-		assert.NilError(t, err)
+// Helper function to create test image folders
+func createTestImageFolder(t *testing.T, baseDir, folderName string) {
+	folderPath := filepath.Join(baseDir, folderName)
+	require.NoError(t, os.MkdirAll(folderPath, 0755))
 
-		testImages = getImagesSortedByModifiedDate(propertiesTest.FolderPath)
-		folderSizeInGiB = getCurrentFolderSizeInGiB(propertiesTest.FolderPath)
-	}
-
-	// Test with a folder that has exceeded the max folder size
-	setupTestFiles("test/img1/img1.squashfs", []byte("test1"))
-	setupTestFiles("test/img2/img2.squashfs", []byte("test2"))
-	testImages = getImagesSortedByModifiedDate(propertiesTest.FolderPath)
-	assert.Equal(t, 1, len(testImages))
-	for i := len(testImages) - 1; folderNeedsCleanup(propertiesTest, folderSizeInGiB, testImages); i-- {
-		err := deleteImage(propertiesTest.FolderPath, testImages[i])
-		assert.NilError(t, err)
-
-		testImages = getImagesSortedByModifiedDate(propertiesTest.FolderPath)
-		folderSizeInGiB = getCurrentFolderSizeInGiB(propertiesTest.FolderPath)
-	}
-
-	cleanupTestDirectories("test")
+	imagePath := filepath.Join(folderPath, "image.squashfs")
+	require.NoError(t, os.WriteFile(imagePath, []byte("blub"), 0644))
 }
